3222676c1de657d799eb44492de842ebdc9d1f8e
Brotnkr <21546142@dut4life.ac.za>
2018-07-23 14:26:12 +0200

    header done

--- dist/port.0c168dd3.js
+++ dist/port.0c168dd3.js
@@ -169,7 +169,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\lll.jpg":[["lll.9f7a2c6c.jpg","assets\\images\\lll.jpg"],"assets\\images\\lll.jpg"],"_css_loader":"..\\..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\css-loader.js"}],"..\\..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\oliver-shou-360281-unsplash.jpg":[["oliver-shou-360281-unsplash.30b52d43.jpg","assets\\images\\oliver-shou-360281-unsplash.jpg"],"assets\\images\\oliver-shou-360281-unsplash.jpg"],"_css_loader":"..\\..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\css-loader.js"}],"..\\..\\..\\..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -198,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '35985' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '9191' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
